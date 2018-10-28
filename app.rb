530db848d98693c857519d6c984986e362a118bb
Min Hee  Park <minheekangg@gmail.com>
2018-10-28 21:30:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-    def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    # def call(env)
+    # resp = Rack::Response.new
+    # req = Rack::Request.new(env)
 
 
     get('/name'){resp.write "minhee"}
-  end
+  # end
 
 end
